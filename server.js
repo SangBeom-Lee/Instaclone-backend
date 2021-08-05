1cbdebbc6d610098bb930f747c3b6ce6a601a3bf
SangBeom-Lee <bumeeya2@naver.com>
2021-08-06 00:14:58 +0900

    Insta Backend Setup

---
+++ server.js
@@ -0,0 +1,51 @@
+import { PrismaClient } from "@prisma/client";
+import {ApolloServer, gql} from "apollo-server";
+
+const client                                        = new PrismaClient();
+
+const typeDefs                                      = gql`
+    type Movie {
+        id                                          : Int!
+        title                                       : String!
+        year                                        : Int!
+        genre                                       : String
+        createdAt                                   : String!
+        updatedAt                                   : String!
+    }
+
+    type Query {
+        movies                                      : [Movie]
+        movie                                       : Movie
+    }
+
+    type Mutation {
+        createMovie (title : String!, year : Int!, genre : String)               : Movie
+        deleteMovie (title : String!)               : Boolean
+    }
+`;
+
+const resolvers                                     = {
+    Query                                           : {
+        movies                                      : () => client.movie.findMany(),
+        movie                                       : () => ({title : "Hello", year : 2021})
+    },
+
+    Mutation                                        : {
+        createMovie                                 : (_, {title, year, genre}) => client.movie.create({data: {
+            title,
+            year,
+            genre
+        }}),
+        deleteMovie                                 : (_, args) => {
+            console.log(args);
+            return true
+        }
+    }
+};
+
+const server                                        = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.listen().then(() => console.log("Server is running on http://localhost:4000/"));
