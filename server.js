812a8e8cb879f3e6ab25e0b2e057981f70bce170
SangBeom-Lee <bumeeya2@naver.com>
2021-08-09 23:42:12 +0900

    Insta Recap

--- server.js
+++ server.js
@@ -1,51 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+require("dotenv").config();
 import {ApolloServer, gql} from "apollo-server";
-
-const client                                        = new PrismaClient();
-
-const typeDefs                                      = gql`
-    type Movie {
-        id                                          : Int!
-        title                                       : String!
-        year                                        : Int!
-        genre                                       : String
-        createdAt                                   : String!
-        updatedAt                                   : String!
-    }
-
-    type Query {
-        movies                                      : [Movie]
-        movie                                       : Movie
-    }
-
-    type Mutation {
-        createMovie (title : String!, year : Int!, genre : String)               : Movie
-        deleteMovie (title : String!)               : Boolean
-    }
-`;
-
-const resolvers                                     = {
-    Query                                           : {
-        movies                                      : () => client.movie.findMany(),
-        movie                                       : () => ({title : "Hello", year : 2021})
-    },
-
-    Mutation                                        : {
-        createMovie                                 : (_, {title, year, genre}) => client.movie.create({data: {
-            title,
-            year,
-            genre
-        }}),
-        deleteMovie                                 : (_, args) => {
-            console.log(args);
-            return true
-        }
-    }
-};
+import schema from "./schema";
 
 const server                                        = new ApolloServer({
-    typeDefs,
-    resolvers
+    schema
 });
 
-server.listen().then(() => console.log("Server is running on http://localhost:4000/"));
+const PORT                                          = process.env.PORT;
+
+server.listen(PORT).then(() => console.log(`Server is running on http://localhost:${PORT}/`));
